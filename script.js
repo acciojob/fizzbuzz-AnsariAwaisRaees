e271ff507c69cfff2e1a6d48ad05ebf9aa8ba523
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-25 19:51:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let res = '';
+
+for (let i = 0; i <= 20; i++) {
+	if (i%3 == 0 && i%5 == 0) {
+		res += 'FizzBuzz\n';
+	}else if (i%3 == 0) {
+		res += 'Fizz\n';
+	}else if (i%5 == 0) {
+		res += 'Buzz\n';
+	}else {
+		res += i+"\n";
+	}
+}
+
+alert(res);
